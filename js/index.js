58d12b915759ba10a11b0cd63ec8d85f6d8da32f
ClydeFrog04 <egan.randal@gmail.com>
2020-03-21 09:33:24 -0600

    Started work

--- js/index.js
+++ js/index.js
@@ -39,4 +39,69 @@ const siteContent = {
 
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+logo.setAttribute('src', siteContent["nav"]["img-src"]);
+
+let codeSnip = document.getElementById("cta-img");
+codeSnip.setAttribute('src', siteContent["cta"]["img-src"]);
+
+let buttons = document.getElementsByTagName("button");
+buttons[0].textContent = siteContent["cta"]["button"];
+
+let headings = document.getElementsByTagName("h1");
+headings[0].textContent = siteContent["cta"]["h1"];
+
+let mainContent = document.getElementsByClassName("top-content")[0].childNodes;
+/*
+const filterLargeStates = data.filter((state) => {
+  return state.population >= 650000;
+});
+ */
+// let textContent = features.filter((element) =>{
+//     return element === "div.text-content";
+// });
+// featuresTextContent = [];
+// console.log("Features: ", features);
+// features.forEach((element) =>{
+//     if(element.nodeName === "div") featuresTextContent.push(element);
+// });
+// features[0].textContent = "Hello world!";
+let features = mainContent[1].childNodes;
+let about = mainContent[3].childNodes;
+
+features[1].textContent = siteContent["main-content"]["features-h4"];
+features[3].textContent = siteContent["main-content"]["features-content"];
+about[1].textContent = siteContent["main-content"]["about-h4"];
+about[3].textContent = siteContent["main-content"]["about-content"];
+
+let midImg = document.getElementById("middle-img");
+midImg.setAttribute('src', siteContent["main-content"]["middle-img-src"]);
+
+
+let bottomContent = document.getElementsByClassName("bottom-content")[0].childNodes;
+let services = bottomContent[1].childNodes;
+let product = bottomContent[3].childNodes;
+let vision = bottomContent[5].childNodes;
+
+services[1].textContent = siteContent["main-content"]["services-h4"];
+services[3].textContent = siteContent["main-content"]["services-content"];
+
+product[1].textContent = siteContent["main-content"]["product-h4"];
+product[3].textContent = siteContent["main-content"]["product-content"];
+
+vision[1].textContent = siteContent["main-content"]["vision-h4"];
+vision[3].textContent = siteContent["main-content"]["vision-content"];
+
+
+let contact = document.getElementsByClassName("contact");
+console.log(contact[0]);
+contact[0].getElementsByTagName("h4")[0].textContent = siteContent["contact"]["contact-h4"];
+let contactPs = contact[0].getElementsByTagName("p");
+contactPs[0].textContent = siteContent["contact"]["address"];
+contactPs[1].textContent = siteContent["contact"]["phone"];
+contactPs[2].textContent = siteContent["contact"]["email"];
+console.log(contactPs);
+
+
+let footer = document.getElementsByTagName("footer");
+let footerP = footer[0].getElementsByTagName("p");
+footerP[0].textContent = siteContent["footer"]["copyright"];
